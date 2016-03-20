7a2aa510d205eda2a2620dfb77278fe9adb87393
wxj <wangxiaojianffgz@163.com>
2016-03-20 21:44:03 +0800

    new push

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :rvm_path, '/usr/local/rvm/bin/rvm'
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
-set :shared_paths, ['config/database.yml', 'config/secrets.yml', 'log', 'tmp/pids', 'tmp/sockets']
+set :shared_paths, ['config/database.yml', 'config/secrets.yml', 'log', 'tmp/pids', 'tmp/sockets', 'public/system']
 
 # Optional settings:
 #   set :user, 'foobar'    # Username in the server to SSH to.
